98ba3768f6bc815a2584e3f61f34c92790c6a18a
hareesh <hareesh.vanapalli.888@gmail.com>
2023-05-09 18:18:56 +0530

    [Hareesh]

--- Employee Wage Calculation/Program.cs
+++ Employee Wage Calculation/Program.cs
@@ -1,29 +1,25 @@
-﻿namespace Employee_Wage_Calculation
+﻿class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        //constants
+        int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        //Variables
+        int empHrs = 0;
+        int empWage = 0;
+        Random random = new Random();
+        //Computation
+        int empCheck = random.Next(0, 2);
+        if (empCheck == IS_FULL_TIME)
         {
-            Console.WriteLine("Employee Wage Calculation");
-
-            int IS_FULL_TIME = 1;
-            Random random = new Random();
-            //Computation
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
-            {
-                Console.WriteLine("Employee is Present");
-            }
-            else
-            {
-                Console.WriteLine("Employee is Present");
-            }
+            empHrs = 8;
         }
+        else
+        {
+            empHrs = 0;
+        }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        Console.WriteLine("Emp Wage : " + empWage);
     }
 }
-
-
-
-
-
-
