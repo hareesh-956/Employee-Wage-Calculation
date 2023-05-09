a61232bdf7315537d29a4c85751aa6e4325285a9
hareesh <hareesh.vanapalli.888@gmail.com>
2023-05-09 16:18:40 +0530

    [Hareesh]

--- Employee Wage Calculation/Program.cs
+++ Employee Wage Calculation/Program.cs
@@ -5,6 +5,25 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Employee Wage Calculation");
+
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Present");
+            }
         }
     }
 }
+
+
+
+
+
+
