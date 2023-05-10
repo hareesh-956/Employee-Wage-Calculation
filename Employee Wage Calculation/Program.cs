cf981f4bd7fce42b845664bf6c268386d2f55d00
hareesh <hareesh.vanapalli.888@gmail.com>
2023-05-10 10:57:44 +0530

    [Hareesh]

--- Employee Wage Calculation/Program.cs
+++ Employee Wage Calculation/Program.cs
@@ -3,6 +3,7 @@
     static void Main(string[] args)
     {
         //constants
+        int IS_PART_TIME = 1;
         int IS_FULL_TIME = 1;
         int EMP_RATE_PER_HOUR = 20;
         //Variables
@@ -10,14 +11,18 @@
         int empWage = 0;
         Random random = new Random();
         //Computation
-        int empCheck = random.Next(0, 2);
-        if (empCheck == IS_FULL_TIME)
+        int empCheck = random.Next(0, 3);
+        if (empCheck == IS_PART_TIME)
+        {
+            empHrs = 4;
+        }
+        else if (empCheck == IS_FULL_TIME)
         {
             empHrs = 8;
         }
         else
         {
-            empHrs = 0;
+            empHrs = 8;
         }
         empWage = empHrs * EMP_RATE_PER_HOUR;
         Console.WriteLine("Emp Wage : " + empWage);
