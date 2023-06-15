ed8673c866a14f7a1aa093a652f079fb8ee75179
hareesh <hareesh.vanapalli.888@gmail.com>
2023-06-15 19:13:33 +0530

    [Hareesh]

--- Employee Wage Calculation/Program.cs
+++ Employee Wage Calculation/Program.cs
@@ -1,38 +1,16 @@
-﻿class Program
+﻿namespace Employee_Wage_Calculation
 {
-    public const int IS_PART_TIME = 1;
-    public const int IS_FULL_TIME = 2;
-    public const int EMP_RATE_PER_HOUR = 20;
-    public const int NUM_OF_WORKING_DAYS = 2;
-    public const int MAX_HRS_IN_MONTH = 10;
-    static void Main(string[] args)
+    internal class Program
     {
-        //Variables
-        int empHrs = 0;
-        int totalEmpHrs = 0;
-        int totalWorkingDays = 0;
-        //Computation
-        while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+        static void Main(string[] args)
         {
-            totalWorkingDays++;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
-            {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
-            }
-            totalEmpHrs += empHrs;
-            Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hrs : " + empHrs);
+            Console.WriteLine("Welcome to Employee Wage Computation Program!");
+
+            Class1 employee = new Class1();
+            int totalWage = employee.ComputeWage();
+            Console.WriteLine("\n\tEmployee Wage is: " + totalWage);
+
+            Console.ReadLine();
         }
-        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-        Console.WriteLine("Total Emp Wage : " + totalEmpWage);
     }
 }
